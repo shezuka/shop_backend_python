2d1aead68de5c16c8f3599880b2485733d26a228
Dima <dima.horbal.dev@gmail.com>
2024-04-18 19:33:03 -0600

    add database sql schema dump

--- backend_python/app/helpers/password.py
+++ backend_python/app/helpers/password.py
@@ -5,12 +5,12 @@ def hash_password(in_password: str) -> str:
     sha256 = hashlib.sha256()
     sha256.update(in_password.encode('utf-8'))
 
-    ripemd160 = hashlib.md5()
-    ripemd160.update(sha256.digest())
+    md5 = hashlib.md5()
+    md5.update(sha256.digest())
     for i in range(0, 128):
-        ripemd160.update(ripemd160.digest())
+        md5.update(md5.digest())
 
-    sha256.update(ripemd160.digest())
+    sha256.update(md5.digest())
     return sha256.hexdigest()
 
 
